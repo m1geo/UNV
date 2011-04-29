d01f50f1e01c72b49b1a033724287ff0ef642979
Aaron Brown <A.H.Brown@warwick.ac.uk>
2011-04-29 18:26:30 +0100

    Un fail

--- server/src/mkv.c
+++ server/src/mkv.c
@@ -1,148 +1,133 @@
-#include "gcs.h"
+#include "util.h"
 
-    AVOutputFormat *fmt;
-    AVFormatContext *oc;
-    AVStream *audio_st, *video_st;
-    double audio_pts, video_pts;
+
+	AVCodecContext  			*pCodecCtxVidEncMKV;
+	AVCodec						*pCodecVidEncMKV;
+	AVFrame						*pFrameEncMKV;
+
+    AVOutputFormat *MKVfmt;
+    AVFormatContext *MKVoc;
+    AVStream *MKV_audio_st, *MKV_video_st;
+    double MKV_audio_pts, MKV_video_pts;
     int i;
     
     long int	iFrame = 0;
+    
+void mkv_all_init(int width, int height, int pix_fmt) {
 
-	printf("This program attempts to make %f seconds of video/audio from webcam\n", STREAM_DURATION);
-
-    /* auto detect the output format from the name. default is
-       mpeg. */
-    fmt = av_guess_format(NULL, filename, NULL);
-    if (!fmt) {
-        printf("Could not deduce output format from file extension: using MPEG.\n");
-        fmt = av_guess_format("mpeg", NULL, NULL);
-    }
-    if (!fmt) {
-        fprintf(stderr, "Could not find suitable output format\n");
-        exit(1);
-    }
+    MKVfmt = av_guess_format("mkv", NULL, NULL);
+    if (!MKVfmt) {
+		fprintf(stderr, "Couldn;t setup format\n");
+		exit(EXIT_FAILURE);
+	}
 
     /* allocate the output media context */
-    oc = avformat_alloc_context();
-    if (!oc) {
+    MKVoc = avformat_alloc_context();
+    if (!MKVoc) {
         fprintf(stderr, "Memory error\n");
         exit(1);
     }
-    oc->oformat = fmt;
-    snprintf(oc->filename, sizeof(oc->filename), "%s", filename);
+    MKVoc->oformat = MKVfmt;
+    snprintf(MKVoc->filename, sizeof(MKVoc->filename), "%s", filename);
 
     /* add the audio and video streams using the default format codecs
        and initialize the codecs */
-    video_st = NULL;
-    audio_st = NULL;
-    if (fmt->video_codec != CODEC_ID_NONE) {
-        video_st = add_video_stream(oc, fmt->video_codec);
+    MKV_video_st = NULL;
+    MKV_audio_st = NULL;
+    if (MKVfmt->video_codec != CODEC_ID_NONE) {
+        MKV_video_st = add_video_stream(MKVoc, MKVfmt->video_codec);
     }
     if (fmt->audio_codec != CODEC_ID_NONE) {
-        audio_st = add_audio_stream(oc, fmt->audio_codec);
+        MKV_audio_st = add_audio_stream(MKVoc, MKVfmt->audio_codec);
     }
 
     /* set the output parameters (must be done even if no
        parameters). */
-    if (av_set_parameters(oc, NULL) < 0) {
+    if (av_set_parameters(MKVoc, NULL) < 0) {
         fprintf(stderr, "Invalid output format parameters\n");
         exit(1);
     }
 
-    dump_format(oc, 0, filename, 1);
+   // dump_format(MKVoc, 0, filename, 1);
 
     /* now that all the parameters are set, we can open the audio and
        video codecs and allocate the necessary encode buffers */
-       
-    open_webcam();  
     
+        
     if (video_st)
-        open_video(oc, video_st);
+        open_video(MKVoc, MKV_video_st);
     if (audio_st)
-        open_audio(oc, audio_st);
+        open_audio(MKVoc, MKV_audio_st);
 
 	
     /* open the output file, if needed */
     if (!(fmt->flags & AVFMT_NOFILE)) {
-        if (url_fopen(&oc->pb, filename, URL_WRONLY) < 0) {
+        if (url_fopen(&MKVoc->pb, filename, URL_WRONLY) < 0) {
             fprintf(stderr, "Could not open '%s'\n", filename);
             exit(1);
         }
     }
 
     /* write the stream header, if any */
-    av_write_header(oc);
-    //addFrameByFile(filename,"header");
-    //addHeader(pHead) //OABDA - How? what is oc?
+    av_write_header(MKVoc);
     
-    usleep(30000000);
-    for(;;) {
-		printf("\rFrame %5ld  ", iFrame);
+}
+
+uint8_t *encode_frame_to_mkv(AVFrame * pFrameRaw)
         /* compute current audio and video time */
-        if (audio_st)
-            audio_pts = (double)audio_st->pts.val * audio_st->time_base.num / audio_st->time_base.den;
+        if (MKV_audio_st)
+            MKV_audio_pts = (double)MKV_audio_st->pts.val * MKV_audio_st->time_base.num / MKV_audio_st->time_base.den;
         else
-            audio_pts = 0.0;
+            MKV_audio_pts = 0.0;
 
-        if (video_st)
-            video_pts = (double)video_st->pts.val * video_st->time_base.num / video_st->time_base.den;
+        if (MKV_video_st)
+            MKV_video_pts = (double)MKV_video_st->pts.val * MKV_video_st->time_base.num / MKV_video_st->time_base.den;
         else
-            video_pts = 0.0;
+            MKV_video_pts = 0.0;
 
-        if ((!audio_st || audio_pts >= STREAM_DURATION) &&
-            (!video_st || video_pts >= STREAM_DURATION))
+        if ((!MKV_audio_st || MKV_audio_pts >= STREAM_DURATION) &&
+            (!MKV_video_st || MKV_video_pts >= STREAM_DURATION))
             break;
 
         /* write interleaved audio and video frames */
-        if (!video_st || (video_st && audio_st && audio_pts < video_pts)) {
-            write_audio_frame(oc, audio_st);
+        if (!MKV_video_st || (MKV_video_st && MKV_audio_st && MKV_audio_pts < MKV_video_pts)) {
+            write_audio_frame(MKVoc, MKV_audio_st);
         } else {
-            write_video_frame(oc, video_st);
+            write_video_frame(MKVoc, MKV_video_st);
         }
-        iFrame++;
-    }
-	printf("\n");
-	
-    /* write the trailer, if any.  the trailer must be written
+        MKVoc
+}	
+   
+void close_mpeg_all() {
+	 /* write the trailer, if any.  the trailer must be written
      * before you close the CodecContexts open when you wrote the
      * header; otherwise write_trailer may try to use memory that
      * was freed on av_codec_close() */
     //av_write_trailer(oc);
-
     /* close each codec */
-    if (video_st)
-        close_video(oc, video_st);
-    if (audio_st)
-        close_audio(oc, audio_st);
+    if (MKV_video_st)
+        close_video(MKVoc, MKV_video_st);
+    if (MKV_audio_st)
+        close_audio(MKVoc, MKV_audio_st);
         
     close_webcam();
 
     /* free the streams */
-   
-
-
-
-
-} 
-
-
-static cleanup_mkv() {
-	for(i = 0; i < (int)oc->nb_streams; i++) {
+    for(i = 0; i < (int)oc->nb_streams; i++) {
         av_freep(&oc->streams[i]->codec);
         av_freep(&oc->streams[i]);
     }
-   if (!(fmt->flags & AVFMT_NOFILE)) {
+
+    if (!(fmt->flags & AVFMT_NOFILE)) {
         /* close the output file */
         url_fclose(oc->pb);
     }
 
     /* free the stream */
-    av_free(oc);
+    av_free(MKV_oc);
 
     //return 0;
     dontDie();
-    
 }
-
 ////////////////////////////////////////// GEORGES MKV PART END ///////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
