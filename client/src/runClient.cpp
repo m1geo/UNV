1c188624fd7360cc2a81d89bbc09b5c3ae20bf8b
George Smart <george.smart1@btinternet.com>
2011-04-30 16:21:37 +0100

    removed C start program for bash script and sdjusted make file

--- client/src/runClient.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <cstdio>
-#include <cstdlib>
-
-int main(int argc, char *argv[])
-{
-    system("./exec/client | ./exec/player -");
-    //system("./Client | ./player -");
-}
