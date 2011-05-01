2eb8c7720ce58a4426ca004f11989c02415af0cd
Aaron Brown <A.H.Brown@warwick.ac.uk>
2011-05-01 19:36:23 +0100

    Modified server to have alternate sources

--- server/src/server.c
+++ server/src/server.c
@@ -24,26 +24,87 @@
 #include <libavdevice/avdevice.h> 
 #include <libswscale/swscale.h>
 
-int main(int argc, char *argv[]) {
+FILE * infile;
 
-	AVOutputFormat *fmt;
-    AVFormatContext *oc;
-    AVStream *audio_st, *video_st;
-    double audio_pts, video_pts;
-    long int diff;
-    unsigned char * old_buf_ptr;
-    int i;
-    
-    long int iFrame = 0;
-	setup_ffmpeg();
+/* Globals for Live capture - As i'm too lazy to pass them around*/
+AVOutputFormat *fmt;
+AVFormatContext *oc;
+AVStream *audio_st, *video_st;
+double audio_pts, video_pts;
+
+
+
+int send_packet_from_file(FILE * inf) {
+	char * buffer;
+	int size;
+	int len;
+	size = 12 * 1e3;
 	
-	// Read CLI Options, and error if not vaild
-	if ( (argc<=1)||(get_options(argc, argv)) ) {
-		fprintf(stderr, "error parsing CLI options - couldn't understand options. Run with --help.");
-		exit(EXIT_FAILURE);
+	buffer = malloc(size * sizeof(char));
+	len = fread(buffer, 1, size, inf);
+	addFrame(buffer, size);
+	if (feof(inf)) {
+		len = -1;
 	}
+	return(len);
+}
 
-    fmt = av_guess_format(NULL, filename, NULL);
+unsigned char * send_live_packet(unsigned char * old_buf_ptr){
+	long int diff;
+ if (audio_st)
+            audio_pts = (double)audio_st->pts.val * audio_st->time_base.num / audio_st->time_base.den;
+        else
+            audio_pts = 0.0;
+
+        if (video_st)
+            video_pts = (double)video_st->pts.val * video_st->time_base.num / video_st->time_base.den;
+        else
+            video_pts = 0.0;
+
+        if ((!audio_st || audio_pts >= STREAM_DURATION) &&
+            (!video_st || video_pts >= STREAM_DURATION))
+            return(NULL);
+
+        /* write interleaved audio and video frames */
+        if (!video_st || (video_st && audio_st && audio_pts < video_pts)) {
+            write_audio_frame(oc, audio_st);
+        } else {
+            write_video_frame(oc, video_st);
+        }
+        
+        // Write it to the network.
+        
+        // THIS PIECE OF CODE TOO ABOUT 20 HOURS TO GET RIGHT
+        // ANYBODY BREAKS THIS, AND I WILL KILL THEM
+        //    GS, 2:26AM, 1ST MAY 2011
+		diff = (oc->pb->buf_ptr - old_buf_ptr);
+		if (oc->pb->buf_ptr == oc->pb->buffer) { // buffer got flushed
+			old_buf_ptr = oc->pb->buf_ptr;
+		} else if (diff < 0) { // wrapped buffer
+			addFrame((char *) old_buf_ptr,  oc->pb->buf_end - old_buf_ptr);
+			addFrame((char *) oc->pb->buffer, oc->pb->buf_ptr - oc->pb->buffer);
+			old_buf_ptr = oc->pb->buf_ptr;
+		} else if (diff > 0) { // continued buffer
+			addFrame((char *) old_buf_ptr, diff);
+			old_buf_ptr = oc->pb->buf_ptr;
+		}
+		return(old_buf_ptr);
+}
+
+
+void setup_file_source() {
+	infile = fopen(cliOpts.devicepath, "rb");
+	if (infile == NULL) {
+		fprintf(stderr, "Couldn't open %s for reading\n", cliOpts.devicepath);
+	}
+		
+}
+
+
+void setup_webcam_source() {
+		setup_ffmpeg();
+    
+	    fmt = av_guess_format(NULL, filename, NULL);
     
     if (!fmt) {
         fprintf(stderr, "Could not find suitable output format\n");
@@ -96,59 +157,12 @@ and initialize the codecs */
         }
     }
     
-    startServerRTSP(20,30015,3015);
-    printf("Waiting for 10 seconds for client to connect, before starting.\n");
-	sleep(10);	// allow client to connect before the server starts, so that client receives header!
-	
-    /* write the stream header, if any */
-    av_write_header(oc);
-	addFrame((char *) oc->pb->buffer, 552);	// send the header...
-	iFrame =0;
-
-	old_buf_ptr = oc->pb->buf_ptr;
-    for(;;) {
-		printf("\rFrame %5ld", iFrame);
-        /* compute current audio and video time */
-        if (audio_st)
-            audio_pts = (double)audio_st->pts.val * audio_st->time_base.num / audio_st->time_base.den;
-        else
-            audio_pts = 0.0;
-
-        if (video_st)
-            video_pts = (double)video_st->pts.val * video_st->time_base.num / video_st->time_base.den;
-        else
-            video_pts = 0.0;
-
-        if ((!audio_st || audio_pts >= STREAM_DURATION) &&
-            (!video_st || video_pts >= STREAM_DURATION))
-            break;
-
-        /* write interleaved audio and video frames */
-        if (!video_st || (video_st && audio_st && audio_pts < video_pts)) {
-            write_audio_frame(oc, audio_st);
-        } else {
-            write_video_frame(oc, video_st);
-        }
         
-        // Write it to the network.
-        
-        // THIS PIECE OF CODE TOO ABOUT 20 HOURS TO GET RIGHT
-        // ANYBODY BREAKS THIS, AND I WILL KILL THEM
-        //    GS, 2:26AM, 1ST MAY 2011
-		diff = (oc->pb->buf_ptr - old_buf_ptr);
-		if (oc->pb->buf_ptr == oc->pb->buffer) { // buffer got flushed
-			old_buf_ptr = oc->pb->buf_ptr;
-		} else if (diff < 0) { // wrapped buffer
-			addFrame((char *) old_buf_ptr,  oc->pb->buf_end - old_buf_ptr);
-			addFrame((char *) oc->pb->buffer, oc->pb->buf_ptr - oc->pb->buffer);
-			old_buf_ptr = oc->pb->buf_ptr;
-		} else if (diff > 0) { // continued buffer
-			addFrame((char *) old_buf_ptr, diff);
-			old_buf_ptr = oc->pb->buf_ptr;
-		}
-        iFrame++;
-    }
-	
+}
+
+void cleanup() {
+	int i;
+	if (strcmp(cliOpts.mode, "LIVE")) {
 	// write trailer
     av_write_trailer(oc);
 	addFrame((char *) oc->pb->buffer, oc->pb->buf_ptr - oc->pb->buffer);	// send the trailer...
@@ -172,7 +186,54 @@ and initialize the codecs */
     }
     
     av_free(oc);
+	} else if (strcmp(cliOpts.mode, "FILE")) {
+		fclose(infile);
+	}
+	printf("Clean exit, Champagne time\n");
+}
+
+int main(int argc, char *argv[]) {
+    unsigned char * old_buf_ptr;
+    int i;
+    long int iFrame = 0;
+    
+
 	
-	printf("Clean Exit\n");
+	// Read CLI Options, and error if not vaild
+	if ( (argc<=1)||(get_options(argc, argv)) ) {
+		fprintf(stderr, "error parsing CLI options - couldn't understand options. Run with --help.");
+		exit(EXIT_FAILURE);
+	}
+
+        
+  
+    startServerRTSP(20,30015,3015);
+    printf("Waiting for 10 seconds for client to connect, before starting.\n");
+	sleep(10);	// allow client to connect before the server starts, so that client receives header!
+	
+	
+	if (strcmp(cliOpts.mode, "WEBCAM")) {
+		/* write the stream header, if any */
+		av_write_header(oc);
+		addFrame((char *) oc->pb->buffer, 552);	// send the header...
+		iFrame =0;
+
+		old_buf_ptr = oc->pb->buf_ptr;
+	}
+    for(;;) {
+		
+		printf("\rFrame %5ld", iFrame);
+        if(strcmp(cliOpts.mode, "FILE")) {
+			if(send_packet_from_file(infile) == -1) break;
+		} else {
+			old_buf_ptr = send_live_packet(old_buf_ptr);
+			if (old_buf_ptr == NULL)
+			break;
+		}
+		
+        iFrame++;
+    }
+	
+	cleanup();
 	exit(EXIT_SUCCESS);
 }
