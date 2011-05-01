eb63a74c813439028b41b254dc8dffe823a6101f
George Smart <george.smart1@btinternet.com>
2011-05-01 20:36:52 +0100

    Updated the command line parameters so the variables are now filled correctly.
    Server.c needs editing to reflect the change from 'devicepath' to 'url'.

--- server/src/util.c
+++ server/src/util.c
@@ -36,6 +36,8 @@ int 						swsR_YUV;
 int 						iPktSize;
 struct cliOpts_t 			cliOpts;
 
+#define printOpts(a,b,c) printf("\t-%s\t--%s\t%s\n",a,b,c)
+
 static const char *optString = "vf:r:m:u:p:h";
 
 static const struct option longOpts[] = {
@@ -51,21 +53,13 @@ static const struct option longOpts[] = {
 
 struct cliOpts_t 
 {	
-	int     verbose;    // if ! false, show verbose messages
-	int     version;    // if ! false, show version message
-	int     formats;    // if ! false, show supported formats
-	int      saveframes;    // if ! false, save every <val>th frame to pmm
-	int      width;      // width of image, if non 0
-	int     height;      // height of image, if non 0
-	int     maxframes;    // only process <val> frames
-	int      CRASHOUT;    // DO NOT USE! DEVELOPMENT ONLY
-	int         saveaudio;      // TRUE if audiopath is defined (used to decide if to open file handles, etc)
-	int      savevideo;    // TRUE if videopath is defined (used to decide if to open file handles, etc)
-	int      networkport;  // Port for Network
-	char*    devicepath;      // path to file/device
-	char*    mode;      // mode for program
-	char*    videopath;    // Path for output video	
-	char*       audiopath;      // Path for output audio
+	int     verbose;      // if ! false, show verbose messages
+	int     formats;      // if ! false, show supported formats
+	int     width;        // width of image, if non 0
+	int     height;       // height of image, if non 0
+	int     networkport;  // Port for Network
+	char*   url;          // path to file/device
+	char*   mode;         // mode for program
 };
 
 AVFrame *alloc_picture(enum PixelFormat pix_fmt, int width, int height)
@@ -327,19 +321,16 @@ void show_format (AVInputFormat * InFmt) {
 void usage (char prog[]) {
 	printf("Usage:\t%s [options]\n", prog);
 	printf("Options:\n");
-	printf("\t-h\t--help\n\t\tPrints this message\n");
-	printf("\t-v\t--verbose\n\t\tEnable verbose output\n");
-	printf("\t(none)\t--version\n\t\tPrint version information\n");
-	printf("\t-f\t--formats\n\t\tShow supported codecs and drivers\n");
-	printf("\t-s <n>\t--save <n>\n\t\tSave every <n>-th frame to disk\n");
-	printf("\t-z <n>\t--maxframes <n>\n\t\tStop after <n> frames\n");
-	printf("\t-r <widthxheight>\t--resolution <widthxheight>\n\t\tResolution of Webcam (V4L2 mode only)\n");
-	printf("\t-m <mode>\t--mode <mode>\n\t\tWhere <mode> is either:\n\t\t  FILE\tRead file specified with -d option as video source\n\t\t  V4L2\tOpen capture device specified by -d option (eg Webcam)\n");
-	printf("\t-d <input path>\t--device <input path>\n\t\tPath to device (V4L2 mode) or file (FILE mode)\n");
-	printf("\t-o <video path>\t--video <input path>\n\t\tPath of video to be output\n");
-	printf("\t-p <port>\t--port <port>\n\t\tWhere <port> is TCP port number\n");
-
-	exit(EXIT_FAILURE);
+	
+	printOpts("v", "verbose", "Show messages during runtime");
+	printOpts("f", "formats", "Print supported formats for input");
+	printOpts("r", "resolution", "Camera open resolution [e.g. 640x480]");
+	printOpts("m", "mode", "\tMode [file or live]");
+	printOpts("u", "url", "\tPath to URL [file or camera path]");
+	printOpts("p", "port", "\tPort Number for TCP/UDP");
+	printOpts("h", "help", "\tPrint this information");
+	
+	exit(EXIT_SUCCESS);
 }
 
 int get_options (int c, char ** v) {
@@ -351,11 +342,9 @@ int get_options (int c, char ** v) {
 	/* Initialize cliOpts before we get to work. */
 	cliOpts.verbose		= FALSE;
 	cliOpts.formats		= FALSE;
-	cliOpts.saveframes	= 0;
-	cliOpts.maxframes	= 0;
 	cliOpts.width		= 640;	// default
 	cliOpts.height		= 480;	// default
-	cliOpts.devicepath	= NULL;
+	cliOpts.url	= NULL;
 	cliOpts.mode		= NULL;
 	cliOpts.networkport = 0;	// not valid
 
@@ -369,19 +358,11 @@ int get_options (int c, char ** v) {
 				cliOpts.verbose++;
 				break;
 				
-			case 's':	// save frames
-				cliOpts.saveframes = atoi(optarg);
-				break;
-				
-			case 'z':	// maximum  frames
-				cliOpts.maxframes = atoi(optarg);
-				break;
-				
 			case 'f':	// formats
 				cliOpts.formats = TRUE;
 				break;
 				
-			case 'r':	// resoution
+			case 'r':	// resolution
 				if ( strlen(optarg) <= 0 ) {error=TRUE; break;}
 				cliOpts.width = atoi(strtok (optarg, "x"));
 				cliOpts.height = atoi(strtok (NULL, "x"));
@@ -401,15 +382,9 @@ int get_options (int c, char ** v) {
 				cliOpts.networkport = atoi(optarg);
 				break;
 			
-			case 'd':	// device
+			case 'u':	// url path
 				if ( strlen(optarg) <= 0 ) {error=TRUE; break;}
-				cliOpts.devicepath = optarg;
-				break;
-			
-			case 'o':	// output video path
-				if ( strlen(optarg) <= 0 ) {error=TRUE; break;}
-				cliOpts.videopath = optarg;
-				cliOpts.savevideo = TRUE;
+				cliOpts.url = optarg;
 				break;
 								
 			case 'h':	// help, with fallthrough.
@@ -417,25 +392,6 @@ int get_options (int c, char ** v) {
 				usage(v[0]);
 				break;
 
-			case 0:		// long option without a short arg
-				// version?
-				if( strcmp( "version", longOpts[longIndex].name ) == 0 ) {
-					cliOpts.version = TRUE;
-					break;
-				}
-				
-				if( strcmp( "DEFAULT", longOpts[longIndex].name ) == 0 ) {
-					printf("***DEVELOPMENT: SETTING TEST DEFAULT OPTIONS***\n");
-					cliOpts.maxframes = 100;
-					cliOpts.mode = (char*)"V4L2";
-					cliOpts.devicepath = (char*)"/dev/video0";
-					cliOpts.videopath = (char*)"test_out.mpg";
-					cliOpts.width = 640;
-					cliOpts.height = 480;
-					cliOpts.networkport = 9999;
-					break;
-				}
-				
 			default:
 				// Never happens, but just to be sure...
 				printf("get_options is poorly sick\n");
@@ -448,14 +404,10 @@ int get_options (int c, char ** v) {
 	if (cliOpts.verbose) {
 		printf("Command Line Options: (1=TRUE, 0=FALSE)\n");
 		printf("  Be Verbose:     %d\n", cliOpts.verbose);
-		printf("  Show Version:   %d\n", cliOpts.version);
 		printf("  Show Formats:   %d\n", cliOpts.formats);
-		printf("  SaveFrames:     %d\n", cliOpts.saveframes);
-		printf("  MaxFrames:      %d\n", cliOpts.maxframes);
 		printf("  Width:          %d\n", cliOpts.width);
 		printf("  Height:         %d\n", cliOpts.height);
-		printf("  Device:         %s\n", cliOpts.devicepath);
-		printf("  Video Output:   %d (%s)\n", cliOpts.savevideo, cliOpts.videopath);
+		printf("  URL:            %s\n", cliOpts.url);
 		printf("  Network Port:   %d\n", cliOpts.networkport);
 		printf("  Mode:           %s\n", cliOpts.mode);
 		printf("\n");
